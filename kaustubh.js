1e8436b98fc7821a68b1ba737aa39b963e28c713
Kaustubh Kulkarni <57136442+kaustubhk24@users.noreply.github.com>
2021-06-10 13:40:10 +0530

    fixes

--- kaustubh.js
+++ kaustubh.js
@@ -451,3 +451,31 @@ function renderEditorText(content) {
 }
 
 renderEditorText(textContent);
+
+function StartModel(btn,msg){
+var modal = document.getElementById("myModal");
+var btn = document.getElementById(btn);
+var modl_msgview=document.getElementById("modl_msg");
+modl_msgview.innerHTML=msg;
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+}
+
+
